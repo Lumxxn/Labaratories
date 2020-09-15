bedd3adb2321fb42d4cac71b1d731a99b6f418e6
Lumxxn <65042464+Lumxxn@users.noreply.github.com>
2020-09-15 20:54:43 +0300

    Add files via upload

---
+++ Programms/Programms/Laba 1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Programms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double scobki1 = ((Math.PI / 2) + (3 * a));
+            double skobki2 = ((3 * a) - Math.PI);
+
+            double chisl1 = Math.Sin(scobki1);
+            double znam1 = 1 - Math.Sin(skobki2);
+            double y = chisl1 / znam1;
+            Console.WriteLine(y);
+
+            // double scobki2 = (((5 / 4) * Math.PI) + ((3 / 2) * a));
+            double scobki2 = ((5*Math.PI/4) + (3 * a /2));
+            double z = 1 / Math.Tan(scobki2);
+
+            double g = (y) - (z);
+            Console.WriteLine(z);
+            Console.WriteLine(g);
+        }
+    }
+}   
