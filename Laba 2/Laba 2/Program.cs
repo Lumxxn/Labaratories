508ec0718264b1482c5b7b48decfabb464bd64a4
Lumxxn <65042464+Lumxxn@users.noreply.github.com>
2020-09-15 19:29:22 +0300

    Add files via upload

---
+++ Laba 2/Laba 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Programms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = int.Parse(Console.ReadLine());
+
+            double y = Math.Cos(a) + Math.Sin(a) + Math.Cos(3 * a) + Math.Sin(3 * a);
+
+            double skobki1 = (Math.PI / 4) + 2 * a;
+
+            double z = 2 * Math.Sqrt(2) * Math.Cos(a) * Math.Sin(skobki1);
+
+            double g = (y - z);
+
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(g);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laba 3/Laba 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Laba_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = int.Parse(Console.ReadLine());
+
+            double znam1 = Math.Sin(2 * a) + Math.Sin(5 * a) - Math.Sin(3 * a);
+            double chisl1 = Math.Cos(a) - Math.Cos(3 * a) + Math.Cos(5 * a);
+
+            double y = znam1 / chisl1;
+
+            double z = Math.Tan(3 * a);
+
+            double g = y - z;
+
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(g);
+        }
+    }
+}
